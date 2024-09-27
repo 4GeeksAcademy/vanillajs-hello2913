0dc068a1a75e8c048b5f0f4c67ec0f9641d3da8c
Jyebenes <131881275+Jyebenes@users.noreply.github.com>
2024-09-27 23:34:49 +0000

    Generador excusas

---
+++ app.js
@@ -0,0 +1,49 @@
+window.onload = function(){
+	let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+	let action = ['ate', 'peed', 'crushed', 'broke'];
+	let what = ['my homework', 'my phone', 'the car'];
+	let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  
+  let indice_mayor_who = (who.length - 1);
+  let numero_aleatorio_who = Math.random() * indice_mayor_who;
+  numero_aleatorio_who = Math.round(numero_aleatorio_who);
+  let primera_frase = who[numero_aleatorio_who];
+  
+  let indice_mayor_action = (action.length - 1);
+  let numero_aleatorio_action = Math.random() * indice_mayor_action;
+  numero_aleatorio_action = Math.round(numero_aleatorio_action);
+  let segunda_frase = action[numero_aleatorio_action];
+
+  let indice_mayor_what = (what.length - 1);
+  let numero_aleatorio_what = Math.random() * indice_mayor_what;
+  numero_aleatorio_what = Math.round(numero_aleatorio_what);
+  let tercera_frase = what[numero_aleatorio_what];
+
+  let indice_mayor_when = (when.length - 1);
+  let numero_aleatorio_when = Math.random() * indice_mayor_when;
+  numero_aleatorio_when = Math.round(numero_aleatorio_when);
+  let cuarta_frase = when[numero_aleatorio_when];
+
+  let elemento_html = document.querySelector('#excuse');
+  elemento_html.innerHTML =  primera_frase + ' ' + segunda_frase + ' ' + tercera_frase + ' ' + cuarta_frase;
+};
+
+function generateExcuse() {
+    // Listas de componentes de la excusa
+    let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+    let action = ['ate', 'peed', 'crushed', 'broke'];
+    let what = ['my homework', 'my phone', 'the car'];
+    let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+    
+    // Seleccionar aleatoriamente un elemento de cada lista
+    let randomWho = who[Math.floor(Math.random() * who.length)];
+    let randomAction = action[Math.floor(Math.random() * action.length)];
+    let randomWhat = what[Math.floor(Math.random() * what.length)];
+    let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+    // Construir la excusa
+    let excuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+
+    // Mostrar la excusa en el elemento con id "excuse"
+    document.getElementById('excuse').textContent = excuse;
+}
